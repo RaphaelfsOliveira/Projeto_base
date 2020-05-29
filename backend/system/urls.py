b45f856820ffe887f64a94e6cf116aae5add4cac
Raphael Oliveira <raphaelbrf@gmail.com>
2020-05-29 12:11:48 -0300

    - serializers User and Group - routers User and Group - views User and Group - add rest_framework in settings - add endpoint to api

--- backend/system/urls.py
+++ backend/system/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+V1 = 'v1'
+BASE_URL_V1 = 'api/{}/'.format(V1)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(BASE_URL_V1, include('auth.urls')),
 ]
